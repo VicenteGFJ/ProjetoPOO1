6c9647ba068e65336598a4fdcfe34a0ba793f831
Cecília Assis <cy_assis@hotmail.com>
2015-01-28 17:21:21 -0200

    121

--- teste/src/teste/Teste.java
+++ teste/src/teste/Teste.java
@@ -1,20 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package teste;
 
-/**
- *
- * @author facom89913
- */
+
 public class Teste {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         // TODO code application logic here
     }
